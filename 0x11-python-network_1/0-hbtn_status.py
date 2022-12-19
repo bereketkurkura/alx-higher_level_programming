8c123fc33ceb73a02aa29d487101306b300267fc
Bereket Kurkura <105045286+bereketkurkura@users.noreply.github.com>
2022-12-19 21:14:41 +0300

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python3
-"""A script that
-- fetches https://intranet.hbtn.io/status.
-- uses urlib package
-"""
+import urllib.request
 
+# Fetch the URL
+url = "https://alx-intranet.hbtn.io/status"
 
-if __name__ == '__main__':
-    import urllib.request
+# Use a with statement to open the URL
+with urllib.request.urlopen(url) as response:
+    # Read and decode the response
+    html = response.read().decode()
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    # Split the response into lines
+    lines = html.split("\n")
+
+    # Print each line preceded by a "-"
+    for line in lines:
+        print("-", line)
