e5afe58f246abc43af742e0509e9706ab1e7b5ff
Bereket Kurkura <105045286+bereketkurkura@users.noreply.github.com>
2022-12-19 21:15:19 +0300

    4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+""" a Python script that fetches https://alx-intranet.hbtn.io/status """
+
+
 import requests
 
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+if __name__ == '__main__':
+    res = requests.get("https://alx-intranet.hbtn.io/status").text
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print(f"\t- type: {type(res)}")
+    print(f"\t- content: {res}")
